572975d19faa7e9c72d02842b9e3b9a51ace805c
copiner <810023494@qq.com>
2021-08-04 15:31:20 +0800

    directive

--- src/directive/bgcolor.js
+++ src/directive/bgcolor.js
@@ -1,33 +1,18 @@
 /*
-指令的注册方式和「过滤器」「混入」「组件」注册的方式一样都分为两种：一是全局注册，二是局部注册
+created - new! This is called before the element's attributes or event listeners are applied.
 
+beforeMount- Occurs once the directive is bound to the element. Occurs only once.
 
-Vue 提供了自定义指令的5个钩子函数：
+mounted- Occurs once the element is inserted into the parent DOM.
 
-bind：指令第一次绑定到元素时调用，只执行一次。在这里可以进行一次性的初始化设置。
+beforeUpdate: new! This is called before the element itself is updated, much like the component lifecycle hooks.
 
-inserted：被绑定的元素，插入到父节点的 DOM 中时调用（仅保证父节点存在）。
+Updated - This hook is called once the component and the children have been updated.
 
-update：组件更新时调用。
+beforeUnmount: new! Similar to component lifecycle hooks, this will be called right before an element is unmounted.
 
-componentUpdated：组件与子组件更新时调用。
-
-unbind：指令与元素解绑时调用，只执行一次。
-
------------------------------------------------------------------------
-
-bind()：当指令绑定在 HTML 元素上时触发
-
-inserted()：当指令绑定的元素插入到父节点中的时候触发
-
-update()：当指令绑定的元素状态/样式、内容(这里指元素绑定的 vue 数据) 发生改变时触发
-
-componentUpdated()：当 update() 执行完毕之后触发
-
-unbind()：当指令绑定的元素从 dom 中删除时触发
-
+unmounted - This hook is called once the directive is removed. Also called only once.
 */
-
 export default function(app){
 
   app.directive('bgcolor', {
